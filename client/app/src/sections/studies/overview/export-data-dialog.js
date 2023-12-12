1abc764ef9cec56bedd0d7926822123ab6696cad
christos <it21922@hua.gr>
2023-12-12 16:36:05 +0200

    export fix

--- client/app/src/sections/studies/overview/export-data-dialog.js
+++ client/app/src/sections/studies/overview/export-data-dialog.js
@@ -25,21 +25,37 @@ const participantsData = [
     { id: 'HUA_02', gender: 'Male', date_of_birth: '1990-07-21', height: 175, weight: 80, status: 'registered' },
     // ...additional participant records
   ];
-  const timeSeriesData = {
-    Gyroscope: [
+  const Gyroscope= [
       { timestamp: '2023-01-01T00:00:00Z', x: 0.1, y: 0.2, z: 0.3, timezone: '+5:30', participant: 'HUA_01', device: 'OnePlus8' },
       { timestamp: '2023-01-01T00:01:00Z', x: 0.1, y: 0.2, z: 0.3, timezone: '-4:30', participant: 'HUA_02', device: 'SamsungS22' },
       // ...additional gyroscope records
-    ],
-    Accelerometer: [
+    ];
+    const Accelerometer= [
       { timestamp: '2023-01-01T01:01:00Z', x: 0.4, y: 0.5, z: 0.6, timezone: '-4:30', participant: 'HUA_01', device: 'OnePlus8' },
       { timestamp: '2023-01-01T01:02:00Z', x: 0.4, y: 0.5, z: 0.6, timezone: '-4:30', participant: 'HUA_01', device: 'OnePlus8' },
       { timestamp: '2023-01-01T01:03:00Z', x: 0.4, y: 0.5, z: 0.6, timezone: '+5:30', participant: 'HUA_02', device: 'SamsungS22' },
       { timestamp: '2023-01-01T01:05:00Z', x: 0.4, y: 0.5, z: 0.6, timezone: '+5:30', participant: 'HUA_02', device: 'SamsungS22' },
       // ...additional accelerometer records
     ]
-  };
   
+  const study_details = [{
+    _id: {
+        $oid: "65785575225b7d56b3c9e476"
+    },
+    study_id: "HUA",
+    title: "Test Hua",
+    description: "Test",
+    no_participants: 10,
+    owners: [
+        {
+            id: "65784ea81e63bb4d7637641b",
+            email: "christoszal@gmail.com",
+            full_name: "Christos Zalachoris"
+        }
+    ],
+    study_coordinators: [],
+    study_assistants: []
+}]
   const surveyData = [
     { participant: 'HUA_01', surveyId: 'SUR_01', response: 'Yes' },
     { participant: 'HUA_02', surveyId: 'SUR_01', response: 'No' },
@@ -74,10 +90,10 @@ const ExportDataDialog = ({ onClose }) => {
 
 
   const dataCategories = {
-    'Survey Data': ['Mobile Questionnaires'],
-    'Participant Metrics': ['Participants'],
-    'Timeseries Sensor Data': ['Gyroscope Time Series', 'Accelerometer Time Series', 'Magnetometer Time Series'],
-    'Behavioral Data': ['Bite Detection Count','Steps Granularity', 'Meal Pictures']
+    'Timeseries Data': ['Gyroscope Time Series', 'Accelerometer Time Series', 'Steps Time Series'],
+    'Eating Behavioral Data': ['Bite Detection Count', 'Meal Pictures'],
+    'Survey Data': ['Mobile Questionnaires','REDCap Quessionaires'],
+
   };
   const handleExportTypeChange = (event) => {
     setExportType(event.target.value);
@@ -104,70 +120,69 @@ const ExportDataDialog = ({ onClose }) => {
     }
     setSelectAll(!selectAll);
   };
+// Helper function to convert JSON to CSV
+const jsonToCSV = (json) => {
+  const rows = json.map(item => Object.values(item).join(','));
+  const header = Object.keys(json[0]).join(',');
+  return [header, ...rows].join('\r\n');
+};
 
-  const isCategorySelected = (category) => {
-    return selectedData[category]?.length === dataCategories[category].length;
-  };
+// Helper function to create an Excel workbook from JSON
+const jsonToExcelWorkbook = (json, sheetName) => {
+  const workbook = XLSX.utils.book_new();
+  const worksheet = XLSX.utils.json_to_sheet(json);
+  XLSX.utils.book_append_sheet(workbook, worksheet, sheetName);
+  return workbook;
+};
 
-  const downloadCSV = (data, filename) => {
-    let csvContent = "data:text/csv;charset=utf-8,";
-  
-    data.forEach((rowArray) => {
-      let row = rowArray.join(",");
-      csvContent += row + "\r\n";
-    });
-  
-    var encodedUri = encodeURI(csvContent);
-    var link = document.createElement("a");
-    link.setAttribute("href", encodedUri);
-    link.setAttribute("download", filename);
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  };
+// Function to create a blob from Excel workbook
+const workbookToBlob = (workbook) => {
+  const wbout = XLSX.write(workbook, {bookType:'xlsx', type:'binary'});
+  function s2ab(s) {
+    const buf = new ArrayBuffer(s.length);
+    const view = new Uint8Array(buf);
+    for (let i=0; i<s.length; i++) view[i] = s.charCodeAt(i) & 0xFF;
+    return buf;
+  }
+  return new Blob([s2ab(wbout)], {type:"application/octet-stream"});
+};
 
-  const organizeSurveyDataByParticipant = () => {
-    const organizedData = {};
+ 
+  const handleExportClick = async () => {
+    const zip = new JSZip();
+    
+    // Add root-level files for study and participant details
+    const studyDetailsContent = exportType === 'csv' ? jsonToCSV(study_details) : workbookToBlob(jsonToExcelWorkbook(study_details, 'Study Details'));
+    const participantsContent = exportType === 'csv' ? jsonToCSV(participantsData) : workbookToBlob(jsonToExcelWorkbook(participantsData, 'Participants'));
   
-    surveyData.forEach(survey => {
-      if (!organizedData[survey.participant]) {
-        organizedData[survey.participant] = [];
+    zip.file("study_details." + (exportType === 'csv' ? 'csv' : 'xlsx'), studyDetailsContent);
+    zip.file("participants." + (exportType === 'csv' ? 'csv' : 'xlsx'), participantsContent);
+  
+    // Handle data for each participant
+    for (const participant of participantsData) {
+      const participantId = participant.id;
+  
+      if (exportType === 'csv') {
+        const participantFolder = zip.folder(participantId);
+        participantFolder.file("gyroscope.csv", jsonToCSV(filterTimeSeriesData(Gyroscope, participantId)));
+        participantFolder.file("accelerometer.csv", jsonToCSV(filterTimeSeriesData(Accelerometer, participantId)));
+      } else {
+        // Create a new Excel workbook for each participant
+        const workbook = jsonToExcelWorkbook(filterTimeSeriesData(Gyroscope, participantId), 'Gyroscope');
+        XLSX.utils.book_append_sheet(workbook, XLSX.utils.json_to_sheet(filterTimeSeriesData(Accelerometer, participantId)), 'Accelerometer');
+  
+        // Add the Excel file to the zip
+        const participantBlob = workbookToBlob(workbook);
+        zip.file(`${participantId}.xlsx`, participantBlob);
       }
-      organizedData[survey.participant].push(survey);
-    });
+    }
   
-    return organizedData;
-  };
-  
-
-  const exportToExcel = () => {
-    const workbook = XLSX.utils.book_new();
-  
-    XLSX.utils.book_append_sheet(workbook, XLSX.utils.json_to_sheet(participantsData), 'Participants');
-    XLSX.utils.book_append_sheet(workbook, XLSX.utils.json_to_sheet(timeSeriesData.Gyroscope), 'Gyroscope Time Series');
-    XLSX.utils.book_append_sheet(workbook, XLSX.utils.json_to_sheet(timeSeriesData.Accelerometer), 'Accelerometer Time Series');
-    XLSX.utils.book_append_sheet(workbook, XLSX.utils.json_to_sheet(timeSeriesData.Magnetometer), 'Magnetometer Time Series');
-    XLSX.utils.book_append_sheet(workbook, XLSX.utils.json_to_sheet(surveyData), 'Mobile Questionnaires');
-    XLSX.utils.book_append_sheet(workbook, XLSX.utils.json_to_sheet(biteDetectionData), 'Bite Detection Count');
-    XLSX.utils.book_append_sheet(workbook, XLSX.utils.json_to_sheet(stepsData), 'Steps Granularity');
-    XLSX.utils.book_append_sheet(workbook, XLSX.utils.json_to_sheet(mealPicturesData), 'Meal Pictures');
-  
-    XLSX.writeFile(workbook, 'StudyData.xlsx');
-  };
-  
-  const handleExportClick = () => {
-    if (exportType === 'csv') {
-      if (selectedData['Participant Metrics']) {
-        // Convert participantsData to CSV format and download
-        const csvData = participantsData.map(p => Object.values(p));
-        downloadCSV(csvData, "participants.csv");
-      }
-    }else if (exportType === 'excel') {
-        exportToZip()
-      }
+    // Generate and download the zip file
+    const content = await zip.generateAsync({type: "blob"});
+    saveAs(content, "study_data.zip");
     onClose();
   };
-
+  
   const exportToZip = async () => {
     const zip = new JSZip();
   
@@ -206,8 +221,8 @@ const ExportDataDialog = ({ onClose }) => {
     XLSX.utils.book_append_sheet(workbook, worksheet, sheetName);
   };
   
-  const filterTimeSeriesData = (dataType, participantId) => {
-    return timeSeriesData[dataType].filter(record => record.participant === participantId);
+  const filterTimeSeriesData = (data, participantId) => {
+    return data.filter(item => item.participant === participantId);
   };
   
   const filterDataByParticipant = (data, participantId) => {
@@ -238,20 +253,13 @@ const ExportDataDialog = ({ onClose }) => {
             <MenuItem value="excel">Excel</MenuItem>
           </Select>
         </FormControl>
-
-        {/* Select All Checkbox */}
-        <FormControlLabel
-          control={
-            <Checkbox
-              checked={selectAll}
-              onChange={handleSelectAllChange}
-            />
-          }
-          label="Select All"
-        />
+        <Divider sx={{ my: 2 }} />
 
         {/* Data Categories */}
-        <Grid container spacing={2}>
+        <Typography variant="subtitle1" gutterBottom>
+          Select Date Range
+        </Typography>
+        <Grid container spacing={2} sx={{ mb: 2 }}>
         <Grid item xs={6}>
           <TextField
             label="Start Date"
@@ -263,7 +271,7 @@ const ExportDataDialog = ({ onClose }) => {
             }}
           />
         </Grid>
-        <Grid item xs={6}>
+        <Grid item xs={6} >
           <TextField
             label="End Date"
             type="date"
@@ -274,6 +282,21 @@ const ExportDataDialog = ({ onClose }) => {
             }}
           />
         </Grid>
+        </Grid>
+
+        <Divider sx={{ my: 2 }} />
+
+        <Typography variant="subtitle1" gutterBottom>
+          Choose Data Categories
+        </Typography>
+        <FormControlLabel
+          control={<Checkbox checked={selectAll} onChange={handleSelectAllChange} />}
+          label="Select All"
+          sx={{ mb: 2 }}
+        />
+
+<Grid container spacing={2}>
+
           {Object.entries(dataCategories).map(([category, items]) => (
             <Grid item xs={12} sm={6} key={category}>
               <Typography variant="subtitle1">{category}</Typography>
